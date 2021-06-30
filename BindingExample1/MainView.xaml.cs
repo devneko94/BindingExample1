8b870a4c2e03df4a1085a5cc4aa373972fa40323
makoto <ppkotonari@gmail.com>
2021-06-30 10:23:51 +0900

    プロジェクト ファイルを追加します。

---
+++ BindingExample1/MainView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingExample1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainView : Window
+    {
+        private MainViewModel _viewModel = new MainViewModel();
+
+        public MainView()
+        {
+            InitializeComponent();
+            DataContext = _viewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.時刻 = DateTime.Now;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(_viewModel.時刻.ToString("HH:mm:ss:fff"));
+        }
+    }
+}
---
+++ BindingExample1/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingExample1
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private DateTime _時刻 = DateTime.Now;
+
+        public DateTime 時刻
+        {
+            get => _時刻;
+            set
+            {
+                _時刻 = value;
+                OnPropertyChanged(nameof(時刻));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
